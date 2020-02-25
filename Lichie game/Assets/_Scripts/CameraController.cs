ea7208c9a02ae2e926a9997768d7f0247018edb1
ritchie <battboyritchie@hotmail.nl>
2020-02-25 11:58:06 +0100

    Added camera controller. Follows the player

---
+++ Lichie game/Assets/_Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform target;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // LateUpdate is called once per frame, after the update of other scripts
+    void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
